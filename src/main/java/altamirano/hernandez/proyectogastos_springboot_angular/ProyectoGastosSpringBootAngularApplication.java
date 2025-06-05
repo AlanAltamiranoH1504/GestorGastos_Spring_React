be2552a9ffa000dd2129017fc92e0b6e34b685eb
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2025-06-04 18:52:36 -0600

    initial commit

---
+++ src/main/java/altamirano/hernandez/proyectogastos_springboot_angular/ProyectoGastosSpringBootAngularApplication.java
@@ -0,0 +1,13 @@
+package altamirano.hernandez.proyectogastos_springboot_angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGastosSpringBootAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoGastosSpringBootAngularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/altamirano/hernandez/proyectogastos_springboot_angular/ProyectoGastosSpringBootAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package altamirano.hernandez.proyectogastos_springboot_angular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGastosSpringBootAngularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
